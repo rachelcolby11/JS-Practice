b8bb8e7b61b87838b1ddacb681a1dbf49125e44d
Rachel Colby <rachelcolby11@gmail.com>
2015-10-05 23:47:16 -0700

    Noisebridge Week 4 Extra Credit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var x = 1000;
-var y = 234;
-var sum = x + y;
-var message = "The sum of x and y is " + sum + "."
-alert(message);
+// var x = 1000;
+// var y = 234;
+// var sum = x + y;
+// var message = "The sum of x and y is " + sum + "."
+
+alert("The sum of x and y is " + (1000 + 234) + ".");
