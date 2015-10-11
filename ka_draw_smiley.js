bb3e123de3b93b8fa1219e38e6edd4a564e65af6
Rachel Colby <rachelcolby11@gmail.com>
2015-10-10 23:53:12 -0700

    KA SmileyFace Exercise

---
+++ ka_draw_smiley.js
@@ -0,0 +1,36 @@
+//Define SmileyFace constructor function
+var SmileyFace = function(x, y) {
+    this.centerX = x;
+    this.centerY = y;
+};
+
+//Define draw prototype
+SmileyFace.prototype.draw = function() {
+    fill(255, 234, 0);
+    ellipse(this.centerX, this.centerY, 150, 150);
+    fill(0, 0, 0);
+    ellipse(this.centerX-30, this.centerY-30, 20, 20); 
+    ellipse(this.centerX+30, this.centerY-30, 20, 20); 
+    noFill(); 
+    strokeWeight(3);
+    arc(this.centerX, this.centerY+10, 64, 40, 0, 180);
+};
+
+//Define speak prototype
+SmileyFace.prototype.speak = function(message) {
+    text(message, this.centerX+85, this.centerY);
+};
+
+//Create new instances of SmileyFace object; Call draw and speak for each
+var Smiley1 = new SmileyFace(124, 200);
+Smiley1.draw();
+Smiley1.speak("Have a great day!");
+
+var Smiley2 = new SmileyFace(50, 100);
+Smiley2.draw();
+Smiley2.speak("Happy day!");
+
+var Smiley3 = new SmileyFace(204, 314);
+Smiley3.draw();
+Smiley3.speak("Happy day!");
+    
