5c602fc2d7efab99f00c1f94eefa8dd287f9c15b
Rachel Colby <rachelcolby11@gmail.com>
2015-11-01 22:30:03 -0800

    Fix onclick event for Noisebridge Week 5 EC pt 2

--- script.js
+++ script.js
@@ -4,15 +4,11 @@ var createName = function (first, last) {
 
 var askName = function () { 
   var fullName;
-  while (fullName !== "Marilyn Monroe") {
+  //while (fullName !== "Marilyn Monroe") {
     var first = prompt("What is your first name?");
     var last = prompt("What is your last name?");
     fullName = createName(first, last);
     alert("Your name is " + fullName + "!");
-  }
+  //}
 }
 
-askName();
-
-asker = document.getElementById("asker");
-asker.onclick = askName();
