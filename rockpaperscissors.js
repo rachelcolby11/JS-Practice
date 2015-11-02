d12ca2e54a89aa42e36d605a38fcd21e5fea767c
Rachel Colby <rachelcolby11@gmail.com>
2015-11-02 14:08:27 -0800

    Personalize RPS game outcome message for user

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -9,29 +9,29 @@ if (computerChoice < 0.34) {
 };
 console.log("Computer: " + computerChoice);
 
-var compare = function (choice1, choice2) {
-    if (choice1 === choice2) {
-        alert("The result is a tie!");
-    } else if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-            alert("Rock wins!");
-        } else {
-            alert("Paper wins!");
+var compare = function (userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        alert("It's a tie!");
+    } else if (userChoice === "rock") {
+        if (computerChoice === "scissors") {
+            alert("Good choice! Rock smashes scissors. You win!");
+        } else { //computer has chosen paper
+            alert("Sorry, paper covers rock. Paper wins!");
         };
-    } else if (choice1 === "paper") {
-        if (choice2 === "rock") {
-            alert("Paper wins!");
-        } else {
-            alert("Scissors wins!");
+    } else if (userChoice === "paper") {
+        if (computerChoice === "rock") {
+            alert("Good choice! Paper covers rock.You win!");
+        } else { //computer has chosen scissors
+            alert("Sorry, scissors cuts paper. Scissors wins!");
         };
-    } else if (choice1 === "scissors") {    
-        if (choice2 == "rock") {
-            alert("Rock wins!");
-        } else {
-            alert("Scissors wins!");
+    } else if (userChoice === "scissors") {    
+        if (computerChoice === "paper") {
+            alert("Good choice! Scissors cuts paper. You win!");
+        } else { //computer has chosen rock
+            alert("Sorry, rock smashes scissors. Rock wins!");
         };
     } else {
-        userChoice = prompt(choice1 + " is not a valid entry. Do you choose rock, paper or scissors?");
+        userChoice = prompt(userChoice + " is not a valid entry. Do you choose rock, paper or scissors?");
         compare(userChoice,computerChoice);
     };    
 };
