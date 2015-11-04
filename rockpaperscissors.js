2392d680f4438aa196876d5eb869d4a5309cb66f
Rachel Colby <rachelcolby11@gmail.com>
2015-11-03 21:29:21 -0800

    If RPS tie, computer makes new choice too

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,19 +1,30 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-  computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-  computerChoice = "paper";
-} else {
-  computerChoice = "scissors";
+console.log("User: " + userChoice);
+
+var computerChoice;
+var setComputerChoice = function() {
+    var random = Math.random();
+    if (random < 0.34) {
+      computerChoice = "rock";
+    } else if(random <= 0.67) {
+      computerChoice = "paper";
+    } else {
+      computerChoice = "scissors";
+    };
+    console.log("Computer: " + computerChoice);
+    return computerChoice;
 };
-console.log("Computer: " + computerChoice);
+
+setComputerChoice();
 
 var compare = function (userChoice, computerChoice) {
     if (userChoice === computerChoice) {
         if (confirm("It's a tie! Rematch?") === true) {
-            userChoice = prompt("Do you choose rock, paper or scissors?");
-            compare(userChoice,computerChoice);
+            var secondUserChoice = prompt("Do you choose rock, paper or scissors?");
+            console.log("User: " + secondUserChoice);
+            var newComputerChoice = setComputerChoice();
+            
+            compare(secondUserChoice, newComputerChoice);
         } else {
             alert("Ok, let's play again soon :)");
         };
